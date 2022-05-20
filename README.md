d98c8525b9e82b651a2f16ef895891c6fd4e30e1
francoisplet <f.plet@jurisoft.fr>
2022-05-20 14:37:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-yaadmb-cvbaxd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-yaadmb-cvbaxd)
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './main.css';
+import { BrowserRouter } from 'react-router-dom';
+
+let rootElement = document.getElementById('root');
+//ReactDOM.render(<App />, rootElement);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
